7e9ea7578bc292ea4315784eb30aa7daa221590a
Bennai Nabil <40771404+NabilBennai@users.noreply.github.com>
2021-01-15 21:42:19 +0100

    First commit: HTML Content + Data retrieiving + Using Bootstrap 4.5.3 for style.

---
+++ js/app.js
@@ -0,0 +1,71 @@
+function afficher() {
+    let pays = document.querySelector("#choix_pays").value;
+
+    let data = null;
+
+    let xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+
+        if (this.readyState === this.DONE) {
+            let resultatBrut = JSON.stringify(this.responseText);
+
+
+            let obj = JSON.parse(xhr.responseText)[0];
+            let codePays = obj["code"];
+
+            obj["Pays"] = obj["country"];
+            obj["Code pays"] = obj["code"];
+            obj["Cas confirmés"] = obj["confirmed"];
+            obj["Cas remis"] = obj["recovered"];
+            obj["Cas en état critique"] = obj["critical"];
+            obj["Morts"] = obj["deaths"];
+            obj["Dernière modification"] = (new Date(obj["lastChange"])).toLocaleString();
+            obj["Derniere actualisation"] = (new Date(obj["lastUpdate"])).toLocaleString();
+
+            delete obj["confirmed"];
+            delete obj["country"];
+            delete obj["code"];
+            delete obj["recovered"];
+            delete obj["critical"];
+            delete obj["deaths"];
+            delete obj["lastChange"];
+            delete obj["lastUpdate"];
+            delete obj.latitude;
+            delete obj.longitude;
+
+
+            let message = "";
+
+            for (const [key, value] of Object.entries(obj)) {
+                message += (`${key}: ${value}` + "<br>");
+            }
+
+            console.log(codePays);
+            let cheminDrapeau = `../resources/flags/${codePays.toLowerCase()}.png`;
+            let baliseDrapeau = `<img src="${cheminDrapeau}" alt="DRAPEAU" width="100" height="120">`;
+
+            document.getElementById("data").innerHTML = baliseDrapeau + "<br>" + message;
+        }
+    });
+
+
+    xhr.open(
+        "GET",
+        "https://covid-19-data.p.rapidapi.com/country?format=json&name=" + pays
+    );
+
+    xhr.setRequestHeader("x-rapidapi-host", "covid-19-data.p.rapidapi.com");
+
+    xhr.setRequestHeader(
+        "x-rapidapi-key",
+        "d30bfcc9d9msh9017ddb0dc94de0p16a542jsn57f2be62f2fc"
+    );
+
+    xhr.send(data);
+
+    document.getElementById("data").style.display = "block";
+
+}
+
