748d0abfceb4d03492da300020b0d4a730485e6a
Bennai Nabil <40771404+NabilBennai@users.noreply.github.com>
2021-02-28 10:19:36 +0100

    Final refactoring

--- js/get_countries.js
+++ js/get_countries.js
@@ -16,4 +16,5 @@ xhr.open("GET", "https://covid-19-data.p.rapidapi.com/help/countries");
 xhr.setRequestHeader("x-rapidapi-key", "29300cd29emsh7c67153e0b24c73p14f1b8jsn494dc27a69f0");
 xhr.setRequestHeader("x-rapidapi-host", "covid-19-data.p.rapidapi.com");
 
+
 xhr.send(data);
